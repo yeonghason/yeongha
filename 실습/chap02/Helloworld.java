cedbe827399131639cfddcbf628494859a8d95c3
yeonghason <163219050+yeonghason@users.noreply.github.com>
2024-03-13 12:07:01 +0900

    Add files via upload

---
+++ 실습/chap02/Helloworld.java
@@ -0,0 +1,12 @@
+package cha01;
+
+public class Helloworld {
+	
+	public static void main(String[] args) {
+	System.out.println("Hello world!!...");
+	
+	
+	
+	}
+
+}
---
+++ 실습/chap02/VarDemo.java
@@ -0,0 +1,12 @@
+package cha04;
+
+public class VarDemo {
+
+	public static void main(String[] args) {
+		
+	int var =1;
+	var x =1;
+
+	}
+
+}
