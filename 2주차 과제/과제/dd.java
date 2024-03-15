01ecd26c76c0a4e6ada95878c697fd3c6b294790
yeonghason <163219050+yeonghason@users.noreply.github.com>
2024-03-15 18:47:51 +0900

    Add files via upload

---
+++ 2주차 과제/과제/dd.java
@@ -0,0 +1,15 @@
+package rhk2;
+
+import java.util.Scanner;
+
+public class dd {
+     
+	public static void main(String[] args) {
+		Scanner in =new Scanner(System.in);
+		System.out.print("정수를 입력하세요 :");
+		int a=in.nextInt();
+		System.out.print(a+"의 제곱은"+a*a);
+		
+	}
+
+}
