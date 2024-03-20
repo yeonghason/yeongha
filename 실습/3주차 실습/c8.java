38c923af73738f42cd1cee6fde9213bf37eb8b58
yeonghason <163219050+yeonghason@users.noreply.github.com>
2024-03-20 12:20:02 +0900

    Add files via upload

---
+++ 실습/3주차 실습/c8.java
@@ -0,0 +1,20 @@
+package chap1;
+
+public class c8 {
+
+	public static void main(String[] args) {
+		int number = 2;
+		switch (number) {
+		case 3:
+			System.out.print("*");
+
+		case 2:
+			System.out.print("*");
+
+		case 1:
+			System.out.print("*");
+		}
+
+	}
+
+}
