107512cf095f2d1d1667380229b3db48124d4fa8
yeonghason <163219050+yeonghason@users.noreply.github.com>
2024-03-20 12:45:47 +0900

    Add files via upload

---
+++ 실습/3주차 실습/c10.java
@@ -0,0 +1,14 @@
+package chap1;
+
+public class c10 {
+
+	public static void main(String[] args) {
+		echo("안녕!", 3);
+	}
+
+	public static void echo(String s, int n) {
+		for (int i = 0; i < n; i++)
+			System.out.println(s);
+	}
+
+}
