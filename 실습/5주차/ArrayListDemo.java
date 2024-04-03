31ddfe1059f0c245d25892349b0995693fe81736
yeonghason <163219050+yeonghason@users.noreply.github.com>
2024-04-03 12:12:18 +0900

    Add files via upload

---
+++ 실습/5주차/ArrayListDemo.java
@@ -0,0 +1,22 @@
+package mp1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		ArrayList<Integer> scores=new ArrayList<>();
+		int data;
+		int sum=0;
+		while ((data=in.nextInt())>=0)
+			scores.add(data);
+		for(int i=0;i<scores.size();i++)
+			sum+=scores.get(i);
+		
+		System.out.println("평균="+(double)sum/scores.size());
+	}
+
+}
