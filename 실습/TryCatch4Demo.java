c843ca58daa104f5ff73ba32c748c394268d86f3
yeonghason <163219050+yeonghason@users.noreply.github.com>
2024-05-22 12:38:55 +0900

    Add files via upload

---
+++ 실습/TryCatch4Demo.java
@@ -0,0 +1,23 @@
+package wret;
+
+public class TryCatch4Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Reso reso=new Reso();
+		
+		try(reso){
+			reso.show();
+		}catch(Exception e) {
+			System.out.println("예외 처리");
+		}
+	}
+}
+class Reso implements AutoCloseable{
+	void show() {
+		System.out.println("자원 사용");
+	}
+	public void close()throws Exception{
+		System.out.println("자원 닫기");
+	}
+}
