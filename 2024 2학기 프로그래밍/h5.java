15b319021b6b1505f65c36b7d7e4833e46d14f25
yeonghason <163219050+yeonghason@users.noreply.github.com>
2024-10-02 14:52:44 +0900

    Add files via upload

---
+++ 2024 2학기 프로그래밍/h5.java
@@ -0,0 +1,15 @@
+package hh;
+
+public class h5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j;
+		for(i=1;i<10;i++,System.out.println()) {
+			for(j=1;j<10;j++,System.out.print('\t')) {
+				System.out.print(i+"*"+j+"="+i*j);
+			}
+		}
+	}
+
+}
