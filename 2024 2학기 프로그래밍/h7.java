7cd4e92c1b62353437e6907f4626cd1c4edc5fe3
yeonghason <163219050+yeonghason@users.noreply.github.com>
2024-10-02 15:11:19 +0900

    Add files via upload

---
+++ 2024 2학기 프로그래밍/h7.java
@@ -0,0 +1,16 @@
+package hh;
+
+public class h7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j;
+		for(i=1;i<=5;i++) {
+			for(j=1;j<=i;j++,System.out.print(i)) {
+				
+			}
+			System.out.println();
+	}
+
+}
+}
---
+++ 2024 2학기 프로그래밍/h9.java
@@ -0,0 +1,19 @@
+package hh;
+import java.util.Scanner;
+public class h9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		
+		int n=in.nextInt();
+		
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++,System.out.print(j-1)) {
+				
+			}
+			System.out.println();
+		}
+	}
+
+}
